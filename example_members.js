e7fa88d1f4034f4a8e079c2dd12b808f61741ad7
ctbus <ctbushman@gmail.com>
2022-06-02 21:41:11 -0400

    Initial commit

---
+++ example_members.js
@@ -0,0 +1,16 @@
+members = [
+    {
+        "id": 0,
+        "Full Name": "",
+        "Nick Name": "",
+        "mid": 0,
+        "fid": 0,
+        "pids": [0],
+        "gender": "",
+        "Birth": "",
+        "Death": "",
+        "img0": "",
+        "img1": "",
+        "Notes": ""
+    }
+]
